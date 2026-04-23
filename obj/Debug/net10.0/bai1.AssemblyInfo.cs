ed1bd861846be5c21073dae5c0268fe03e7488f0
Thanh Tho <trieuthanhtho28@gmail.com>
2026-04-23 22:36:30 +0700

    web html

--- obj/Debug/net10.0/bai1.AssemblyInfo.cs
+++ obj/Debug/net10.0/bai1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bai1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516e71b45b11dfd3e9fa03a65e1b9d314c4b23b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("bai1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bai1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
